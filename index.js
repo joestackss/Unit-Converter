98f96278c591566c4c13a0283041f85ffd311e46
olamideare <63550820+olamideare@users.noreply.github.com>
2022-08-21 11:28:46 +0100

    Js Algorithm

--- index.js
+++ index.js
@@ -4,42 +4,66 @@ let feetH = document.getElementById("feetH");
 let feetH2 = document.getElementById("feetH2");
 let meterH2 = document.getElementById("meterH2");
 
-
 let literH = document.getElementById("literH");
 let gallonH = document.getElementById("gallonH");
 let gallonH2 = document.getElementById("gallonH2");
 let literH2 = document.getElementById("literH2");
 
-
 let kiloH = document.getElementById("kiloH");
+let poundH = document.getElementById("poundH");
+let poundH2 = document.getElementById("poundH2");
+let kiloH2 = document.getElementById("kiloH2");
 
 let btnConvert = document.getElementById("btn-convert");
 
 btnConvert.addEventListener("click", function () {
-  //   console.log(inputText.value);
-
   let inputToNumber = Number(inputText.value);
 
   meterH.textContent = inputText.value;
+  feetH2.textContent = inputText.value;
 
   literH.textContent = inputText.value;
+  gallonH2.textContent = inputText.value;
+
   kiloH.textContent = inputText.value;
+  poundH2.textContent = inputText.value;
 
   function meterToFeet() {
     let feet = inputToNumber * 3.281;
     feetH.textContent = feet;
   }
 
+  function feetToMeter() {
+    let meter = inputToNumber * 0.305;
+    meterH2.textContent = meter;
+  }
+
   function literToGallon() {
     let gallon = inputToNumber * 0.264;
-    gallonH.textContent = gallon;
+    gallonH.textContent = gallon.toFixed(3);
+  }
+
+  function gallonToLiter() {
+    let liter = inputToNumber * 3.785;
+    literH2.textContent = liter.toFixed(3);
   }
 
   function kilogramToPound() {
-    console.log(inputToNumber * 2.204);
+    let pound = inputToNumber * 2.204;
+    poundH.textContent = pound;
+  }
+
+  function poundToKilogram() {
+    let kilogram = inputToNumber * 0.454;
+    kiloH2.textContent = kilogram;
   }
 
   meterToFeet();
+  feetToMeter();
+
   literToGallon();
+  gallonToLiter();
+
   kilogramToPound();
+  poundToKilogram();
 });
